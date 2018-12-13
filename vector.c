a5b5b183172315da178014fc1b6edc2587cd4727
bischmlb <mathiasbischo@gmail.com>
2018-12-13 16:20:39 +0100

    d

--- vector.c
+++ vector.c
@@ -3,6 +3,7 @@
 
 int main(void)
 {
+
 	printf("hello world\n");
 	return 0;
 };
@@ -11,7 +12,7 @@ int main(void)
 void Vector_ctor(Vector *v){
 	v->usedSpace = 0;
 	v->size = 10;
-	v->parray = (int *)malloc(size * sizeof(int));
+	v->parray = (int *)malloc(v->size * sizeof(int));
 };
 
 void Vector_dtor(Vector *v){
@@ -20,3 +21,10 @@ void Vector_dtor(Vector *v){
 	v->usedSpace = 0;
 	v->size = 0;
 };
+
+void Vector_push_back(Vector *v, void *value){
+	// v[size] = value
+	v->parray[v->usedSpace] = value;
+
+
+}
