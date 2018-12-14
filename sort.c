3c22941bbe863b8291243f04a28234eb648e7e9a
tubnielsen <tubnielsen@gmail.com>
2018-12-14 14:41:37 +0100

    update

--- sort.c
+++ sort.c
@@ -2,6 +2,19 @@
 #include "vector.c"
 #include <stdio.h>
 
+int cmpstr(const void* a, const void* b){
+    const char* aa = *(const char**)a;
+    const char* bb = *(const char**)b;
+    return strcmp(aa,bb);
+}
+
+void printVector(Vector *v){
+    for (int i=0; i < v->usedSpace; i++){
+        void *print = v->parray + i * CELLSIZE;
+        printf("%x \n", &print);
+    }
+}
+
 int main(int argc, char *argv[]){
     Vector v;
     Vector *p = &v;
@@ -14,15 +27,16 @@ int main(int argc, char *argv[]){
     
     while (fgets(buffer, CELLSIZE, fp) != NULL){
         Vector_push_back(p, b);
-        printf("%s", b);
+        //printf("%s", b);
         //puts(buffer);
     }
 
     fclose(fp);
+    printVector(&p);
+    //qsort(p->parray, p->usedSpace, CELLSIZE, cmpstr);
+    //printVector(p);
+
     return 0;
 }
 
-void sortVector(Vector *v){
-    qsort(v->parray, v->usedSpace,CELLSIZE)
 
-}
